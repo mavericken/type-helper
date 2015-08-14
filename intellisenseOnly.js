2389cfd7c049cfdfb1433b0c80ceb6cce106f9c3
rvelde <mavericken@hotmail.com>
2015-08-14 14:48:05 -0400

    fix missing file

---
+++ intellisenseOnly.js
@@ -0,0 +1,16 @@
+﻿
+{
+    //for tricking intellisense into letting us get better intellisense
+    //currently broken...
+
+    var intellisenseOnly;
+    function testForIntellisense(theValue) {
+        /// <param name="theValue" value="false">
+        return theValue;
+    }  
+    
+    intellisenseOnly = testForIntellisense(false);
+    
+    module.exports = intellisenseOnly;
+
+}
